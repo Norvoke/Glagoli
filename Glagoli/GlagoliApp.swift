86c8cfe8aad73d2949a00cdef41371232b3f6f59
Finn Ellingwood <finnellingwood@gmail.com>
2023-09-26 09:06:09 -0400

    Initial Commit

---
+++ Glagoli/GlagoliApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlagoliApp.swift
+//  Glagoli
+//
+//  Created by Finn Ellingwood on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GlagoliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
